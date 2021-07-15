7730525f2a935502cf52d10b555a85fa91bfa19b
eholster <ehols001@odu.edu>
2021-07-14 22:55:56 -0400

    The HTMLdoctest files that disappeared

---
+++ src/test/java/edu/odu/cs/cs350/HTMLdocTest.java
@@ -0,0 +1,40 @@
+package edu.odu.cs.cs350;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HTMLdocTest {
+
+	@Test
+	void testHTMLDoc()
+	{
+		//fail("Not yet implemented");
+		HTMLDocument htmlDoc = new HTMLDocument();
+	}
+	
+	@Test
+	void testPath()
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testScripts()
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testStyles()
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testLinks()
+	{
+		fail("Not yet implemented");
+	}
+	
+}
