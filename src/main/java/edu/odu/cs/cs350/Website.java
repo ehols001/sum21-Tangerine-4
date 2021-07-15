ae5c647e7149efbf5645b2e53d82a79ea4046516
eholster <ehols001@odu.edu>
2021-07-15 15:31:27 -0400

    Added a skeleton of the Website class

--- src/main/java/edu/odu/cs/cs350/Website.java
+++ src/main/java/edu/odu/cs/cs350/Website.java
@@ -1,5 +1,45 @@
 package edu.odu.cs.cs350;
 
 public class Website {
+	/*
+	 * DATA STRUCTURE for PAGEURLs
+	 * DATA STRUCTURE for PAGES
+	 * (potentially combine the two, maybe using ?map?)
+	 */
 
+	//Default constructor for initializing a new Website
+	public Website()
+	{
+		
+	}
+	
+	/*
+	 * Determine whether a page contains the type of 
+	 * HTML content we are looking for
+	 */
+	static boolean containsHTMLContent(/*page*/)
+	{
+		if(/*page contains HTML content*/)
+			return true;
+		else
+			return false;
+	}
+	
+	//Add a page URL to a Container of page URLs
+	public void addPageURL()
+	{
+		
+	}
+	
+	//Add a page to a Container of pages
+	public void addPage()
+	{
+		
+	}
+	
+	//Main method for running the WebAnalysis tool
+	public static void main(String[] args)
+	{
+		
+	}
 }
