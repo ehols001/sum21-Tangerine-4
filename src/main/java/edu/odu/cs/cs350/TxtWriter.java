82c85ea05aa1a107e040c33168e43bd638277614
dbimrose <dbimr001@odu.edu>
2021-07-15 10:25:12 -0400

    Set structure for TxtWriter main and test files

--- src/main/java/edu/odu/cs/cs350/TxtWriter.java
+++ src/main/java/edu/odu/cs/cs350/TxtWriter.java
@@ -1,5 +1,15 @@
 package edu.odu.cs.cs350;
 
 public class TxtWriter {
-
+	
+	void txtFormatter()
+	{
+		//Extract size/path data from Website.java
+	}
+	
+	void writeText()
+	{
+		//Write data extracted by txtFormatter() to YYYYMMDD-hhmmss-summary.txt file
+	}
+	
 }
--- src/test/java/edu/odu/cs/cs350/TestTxtWriter.java
+++ src/test/java/edu/odu/cs/cs350/TestTxtWriter.java
@@ -1,5 +1,26 @@
 package edu.odu.cs.cs350;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 class TestTxtWriter {
-  
+ 
+	@Test
+	void testTxtWriter() {
+		fail("Incomplete");
+		TxtWriter writer = new TxtWriter();
+	}
+	
+	@Test
+	void testTxtFormatter() {
+		fail("Incomplete");
+		
+	}
+	
+	@Test
+	void testWriteText() {
+		fail("Incomplete");
+		
+	}
+	
 }
