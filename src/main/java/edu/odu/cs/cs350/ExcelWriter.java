0c37f6b57d03105608ca28aa126aef8b2ba3d72b
jgedamu <87036877+jgedamu@users.noreply.github.com>
2021-07-15 22:20:08 -0400

    Update ExcelWriter.java

--- src/main/java/edu/odu/cs/cs350/ExcelWriter.java
+++ src/main/java/edu/odu/cs/cs350/ExcelWriter.java
@@ -1,5 +1,23 @@
 package edu.odu.cs.cs350;
 
+import Website.java;
+import HTMLDocument.java;
+import java.util.*;
+
 public class ExcelWriter {
+  
+  private Vector<String> pages = new Vector<String>();
+	private Vector<String> images = new Vector<String>();
+  private Vector<String> css = new Vector<String>();
+  private Vector<String> scripts = new Vector<String>();
+  private Vector<String> intraLinks = new Vector<String>();
+  private Vector<String> internalLinks = new Vector<String>();
+  private Vector<String> externalLinks = new Vector<String>();
+  
+  Workbook wb = new XSSFWorkbook();
+  CreationHelper createHelper = wb.getCreationHelper();
+  Sheet sheet1 = wb.createSheet("summary");
+  
+  
 
 }
