b428798191b66f08b3c2b57d78fc8b68261e1b39
jgedamu <87036877+jgedamu@users.noreply.github.com>
2021-07-16 17:17:02 -0400

    Update TestExcelWriter.java

--- src/test/java/edu/odu/cs/cs350/TestExcelWriter.java
+++ src/test/java/edu/odu/cs/cs350/TestExcelWriter.java
@@ -1,5 +1,75 @@
 package edu.odu.cs.cs350;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import ExcelWriter.java;
+import Website.java;
+import HTMLDocument.java;
+
 class TestExcelWriter {
   
+  Website website = new Website();
+ 
+	@Test
+	void defaultConstructorTest() {
+		fail("Incomplete");
+		
+		ExcelWriter writer = new ExcelWriter();
+	}
+	
+	@Test
+	void parameterizedConstructorTest() {
+		fail("Incomplete");
+		
+		ExcelWriter writer2 = new ExcelWriter(website);
+	}
+	
+	@Test
+	void setPagesTest() 
+	{
+		fail("Incomplete");
+	}
+	
+	@Test
+	void setImagesTest() 
+	{
+		fail("Incomplete");
+	}
+	
+	@Test
+  void setCSSTest() 
+	{
+		fail("Incomplete");
+	}
+	
+	@Test
+  void setScriptsTest() 
+	{
+		fail("Incomplete");
+	}
+	
+	@Test
+  void setIntraLinkTest() 
+	{
+		fail("Incomplete");
+	}
+	
+	@Test
+  void setInternalLinkTest() 
+	{
+		fail("Incomplete");
+	}
+	
+	@Test
+  void setExternalLinkTest() 
+	{
+		fail("Incomplete");
+	}
+	
+	@Test
+	void writeToFileTest() 
+	{
+		fail("Incomplete");
+	}
 }
