4678893d3ebb87c48448b01f77c8257972738df4
jgedamu <87036877+jgedamu@users.noreply.github.com>
2021-07-16 14:42:37 -0400

    Update ExcelWriter.java

--- src/main/java/edu/odu/cs/cs350/ExcelWriter.java
+++ src/main/java/edu/odu/cs/cs350/ExcelWriter.java
@@ -6,18 +6,89 @@ import java.util.*;
 
 public class ExcelWriter {
   
-  private Vector<String> pages = new Vector<String>();
-	private Vector<String> images = new Vector<String>();
+  private Vector<String> pages = new Vector<String>(); 
+  private Vector<String> images = new Vector<String>();
   private Vector<String> css = new Vector<String>();
   private Vector<String> scripts = new Vector<String>();
   private Vector<String> intraLinks = new Vector<String>();
   private Vector<String> internalLinks = new Vector<String>();
   private Vector<String> externalLinks = new Vector<String>();
   
-  Workbook wb = new XSSFWorkbook();
-  CreationHelper createHelper = wb.getCreationHelper();
-  Sheet sheet1 = wb.createSheet("summary");
+  public class ExcelWriter(){
+	  
+  }
   
-  
+  public TxtWriter(Website website) 
+	{	
+		setPages(website);
+		setImages(website);
+	  	setCSS(website);
+	  	setScripts(website);
+	  	setIntraLinks(website);
+	  	setInternalLinks(website);
+	  	setExternalLinks(website);
+	}
+	
+	public Vector<String> getPages()
+	{
+		return pages;
+	}
+	
+	void setPages(Website website)
+	{
+		
+	}
+	
+	public Vector<String> getImages()
+	{
+		return images;
+	}
+	
+	void setImages(Website website)
+	{	
+		
+	}
+	
+	public Vector<String> getCSS
+	{
+		return css;
+	}
+	
+	void setCSS (Website website)
+	{
+		
+	}
+	
+	public Vector<String> getScripts
+	{
+		return scripts;
+	}
+	
+	void setScripts (Website website)
+	{
+		
+	}
+	
+	public Vector<String> getIntraLinks
+	{
+		return intraLinks;
+	}
+	
+	void setIntraLinks (Website website)
+	{
+		return intraLinks;
+	}
+	
+	void setCSS (Website website)
+	{
+		
+	}
+	
+	void writeToFile()
+	{
+		
+	}
+	
+}
 
 }
