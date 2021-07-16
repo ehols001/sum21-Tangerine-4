c53f2d2176e871a66de1df74b59c204060c83114
dbimrose <dbimr001@odu.edu>
2021-07-15 20:17:34 -0400

    Added to TxtWriter and TestTxtWriter

--- src/main/java/edu/odu/cs/cs350/TxtWriter.java
+++ src/main/java/edu/odu/cs/cs350/TxtWriter.java
@@ -1,15 +1,58 @@
 package edu.odu.cs.cs350;
 
+import Website.java;
+import HTMLDocument.java;
+import java.util.*;
+
 public class TxtWriter {
 	
-	void txtFormatter()
+	private Vector<String> sizes = new Vector<String>();
+	private Vector<String> pages = new Vector<String>();
+	
+	public TxtWriter()
 	{
-		//Extract size/path data from Website.java
 	}
 	
-	void writeText()
+	public TxtWriter(Website website) 
+	{	
+		setSizes(website);
+		setPages(website);
+	}
+	
+	public Vector<String> getSizes()
 	{
-		//Write data extracted by txtFormatter() to YYYYMMDD-hhmmss-summary.txt file
+		return sizes;
+	}
+	
+	void setSizes(Website website)
+	{
+		/*
+		for(int i = 0; i < website.Pages.size; i++) 
+		{
+			sizes.add(website.Pages[i].size);
+		}
+		*/
+	}
+	
+	public Vector<String> getPages()
+	{
+		return pages;
+	}
+	
+	void setPages(Website website)
+	{	
+		/*
+		for(int i = 0; i < website.Pages.size; i++) 
+		{
+			pages.add(website.Pages[i].path);
+		}
+		*/
+	}
+	
+	void writeToFile()
+	{
+		//Write data in Vector<String> sizes and Vector<String> pages to YYYYMMDD-hhmmss-summary.txt file
+		//according to conventions
 	}
 	
 }
--- src/test/java/edu/odu/cs/cs350/TestTxtWriter.java
+++ src/test/java/edu/odu/cs/cs350/TestTxtWriter.java
@@ -3,24 +3,43 @@ package edu.odu.cs.cs350;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+import TxtWriter.java;
+import Website.java;
+import HTMLDocument.java;
+
 class TestTxtWriter {
+	
+	Website website = new Website();
  
 	@Test
-	void testTxtWriter() {
+	void defaultConstructorTest() {
 		fail("Incomplete");
+		
 		TxtWriter writer = new TxtWriter();
 	}
 	
 	@Test
-	void testTxtFormatter() {
+	void parameterizedConstructorTest() {
 		fail("Incomplete");
 		
+		TxtWriter writer2 = new TxtWriter(website);
 	}
 	
 	@Test
-	void testWriteText() {
+	void setSizesTest() 
+	{
 		fail("Incomplete");
-		
 	}
 	
+	@Test
+	void setPagesTest() 
+	{
+		fail("Incomplete");
+	}
+	
+	@Test
+	void writeToFileTest() 
+	{
+		fail("Incomplete");
+	}
 }
