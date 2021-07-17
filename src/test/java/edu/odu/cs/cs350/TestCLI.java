d9ebc06638cf683057e98f336b1039f7bf656e7f
cpalm012 <cpalm012@odu.edu>
2021-07-16 21:30:26 -0400

    Add method to CLI class and CLI test class
    Add method for localpath validation and several test methods

---
+++ src/test/java/edu/odu/cs/cs350/TestCLI.java
@@ -0,0 +1,36 @@
+package edu.odu.cs.cs350;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCLI {
+
+	@Test
+	void testIsURLValid() {
+		//fail("Not yet implemented");
+				
+		String testURL = "https://www.google.com";
+		String fakeURL = "ww.google,com";
+		assertTrue(CommandLineInterface.isURLValid(testURL));
+		assertFalse(CommandLineInterface.isURLValid(fakeURL));
+	}
+
+	@Test
+	void testLocalPathAccess() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testUrlToLocalPath() {
+		//fail("Not yet implemented");
+		
+		
+	}
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
