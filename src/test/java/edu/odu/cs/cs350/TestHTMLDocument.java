af42014da6517f5a82617041bf35b582180f3c46
cpalm012 <cpalm012@odu.edu>
2021-07-23 14:25:10 -0400

    Update HTMLDoc to use collections of FileResource and Anchor objects
    Still working on implementing full test coverage for HTMLDoc but this is
    a step towards a better interface overall. Anchor and FileResource being
    implemented makes this clear.

--- src/test/java/edu/odu/cs/cs350/TestHTMLDocument.java
+++ src/test/java/edu/odu/cs/cs350/TestHTMLDocument.java
@@ -13,6 +13,8 @@ import org.junit.Before;
 public class TestHTMLDocument {
 
 	private HTMLDocument html1;
+	private Anchor testLink;
+	private FileResource testImage;
 	
 	private String lfp;
 	
@@ -20,6 +22,14 @@ public class TestHTMLDocument {
 	@Before
 	public void setup() {
 		html1 = new HTMLDocument();
+		testLink = new Anchor("www.test.org", "external");
+		testImage = new FileResource();
+		
+		testImage.setPages(3);
+		testImage.setSize(1024);
+		testImage.setType("img");
+		
+		
 		lfp = new String("localFilePath");
 		html1.setLocalPath(lfp);
 	}
@@ -47,45 +57,47 @@ public class TestHTMLDocument {
 	@Test
 	public void testSetLinks() {
 		//fail("Not yet implemented");
-		html1.setLinks("www.google.com");
+		html1.setLinks(testLink);
 		
 		assertEquals(html1.getLinks().size(), 1);
+		assertEquals(html1.getLinks().get(0), testLink);
 		
 	}
 
 	@Test
 	public void testSetMedia() {
 		//fail("Not yet implemented");
-		html1.setMedia("wwww.test.com/random/video.mp4");
+		html1.setMedia(testImage);
 		
 		assertEquals(html1.getMedia().size(), 1);
+		assertEquals(html1.getMedia().get(0), testImage);
 
 	}
 
 	@Test
 	public void testSetScripts() {
-//		fail("Not yet implemented");
-		html1.setScripts("www.test.com/scripts/javascript.js");
-		
-		assertEquals(html1.getScripts().size(), 1);
+		fail("Not yet implemented");
+//		html1.setScripts("www.test.com/scripts/javascript.js");
+//		
+//		assertEquals(html1.getScripts().size(), 1);
 
 	}
 
 	@Test
 	public void testSetStyleSheets() {
-//		fail("Not yet implemented");
-		html1.setStyleSheets("www.test.com/scripts/script.css");
-		
-		assertEquals(html1.getStyleSheets().size(), 1);
+		fail("Not yet implemented");
+//		html1.setStyleSheets("www.test.com/scripts/script.css");
+//		
+//		assertEquals(html1.getStyleSheets().size(), 1);
 
 	}
 
 	@Test
 	public void testSetMisc() {
-//		fail("Not yet implemented");
-		html1.setMisc("www.test.com/misc/misc.zip");
-		
-		assertEquals(html1.getMisc().size(), 1);
+		fail("Not yet implemented");
+//		html1.setMisc("www.test.com/misc/misc.zip");
+//		
+//		assertEquals(html1.getMisc().size(), 1);
 
 	}
 
