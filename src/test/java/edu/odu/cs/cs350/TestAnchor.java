b138fa64688f720ece6f82646f4504f51add9c14
cpalm012 <cpalm012@odu.edu>
2021-07-23 14:12:13 -0400

    Added interface and tests for Anchor class
    Began work on the interface for Anchor class and associated tests

---
+++ src/test/java/edu/odu/cs/cs350/TestAnchor.java
@@ -0,0 +1,39 @@
+package edu.odu.cs.cs350;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestAnchor {
+
+	@Before
+	public void setUp() {
+	}
+
+	@Test
+	public void testAnchor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetURL() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetURL() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetType() {
+		fail("Not yet implemented");
+	}
+
+}
