357a23a7deeaa8b32581d8adc058025c6a0259ea
dbimrose <dbimr001@odu.edu>
2021-07-24 13:18:37 -0400

    Added documentation to TxtWriter.java

--- src/main/java/edu/odu/cs/cs350/TxtWriter.java
+++ src/main/java/edu/odu/cs/cs350/TxtWriter.java
@@ -7,21 +7,39 @@ public class TxtWriter {
 	private Vector<String> sizes = new Vector<String>();
 	private Vector<String> pages = new Vector<String>();
 	
+	/**
+	 * Constructs a text writer object with empty containers
+	 */
 	public TxtWriter()
 	{
 	}
 	
+	/**
+	 * Constructs a text writer object with containers filled by Website object
+	 * 
+	 * @param website website supplies website image size and path data
+	 */
 	public TxtWriter(Website website) 
 	{	
 		setSizes(website);
 		setPages(website);
 	}
 	
+	/**
+	 * Returns Vector<String> of cumulative image sizes for each page
+	 * 
+	 */
 	public Vector<String> getSizes()
 	{
 		return sizes;
 	}
 	
+	/**
+	 * Stores cumulative image sizes for each page supplied 
+	 * from website
+	 * 
+	 * @param website website supplies website image size data for each page
+	 */
 	void setSizes(Website website)
 	{
 		/*
@@ -32,11 +50,21 @@ public class TxtWriter {
 		*/
 	}
 	
+	/**
+	 * Returns Vector<String> of path names for each page
+	 * 
+	 */
 	public Vector<String> getPages()
 	{
 		return pages;
 	}
 	
+	/**
+	 * Stores path names for each page supplied 
+	 * from website
+	 * 
+	 * @param website website supplies website path names for each page
+	 */
 	void setPages(Website website)
 	{	
 		/*
@@ -47,6 +75,9 @@ public class TxtWriter {
 		*/
 	}
 	
+	/**
+	 * Writes formated Vector<String> sizes and Vector<String> pages to YYYYMMDD-hhmmss-summary.txt file
+	 */
 	void writeToFile()
 	{
 		//Write data in Vector<String> sizes and Vector<String> pages to YYYYMMDD-hhmmss-summary.txt file
