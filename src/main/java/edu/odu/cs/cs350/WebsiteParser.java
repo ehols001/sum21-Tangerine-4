6f5eb1cda76cd0e7c998edf644ba76b5c3c873ee
cpalm012 <cpalm012@odu.edu>
2021-08-02 18:40:00 -0400

    Update parser to match local

--- src/main/java/edu/odu/cs/cs350/WebsiteParser.java
+++ src/main/java/edu/odu/cs/cs350/WebsiteParser.java
@@ -1,6 +1,7 @@
 package edu.odu.cs.cs350;
 
 import java.io.File;
+import java.nio.file.Files;
 import java.io.IOException;
 
 import org.jsoup.Jsoup;
@@ -84,22 +85,44 @@ public class WebsiteParser {
 	 */
 	public void extractMedia(Document doc, HTMLDocument htmldoc)
 	{
-		/*
-		Elements media = doc.select("[src]");
+		Elements media = doc.select("[src]"); //collect a list of all media-like tags containing src on the page
 		
-		for(Element src : media)
+		for(Element src : media) //for each src in the media list
 		{
-			if(src.normalName().equals("img"))
-			{
-				String url = src.attr("abs:src");
-				String strippedRoot = UrlHandler.stripProtocol(Website.getRootUrl());
-				if(url.contains(strippedRoot))
-				{
-					htmldoc.setMedia(src);
-				}
+			if (src.normalName().equals("img")) {
+				
+			/**
+			 * This should return the abs path of the media file but it returns ""
+			 */
+			//String url = src.attr("abs:src"); //get the full path of the media src
+			
+			String url = src.baseUri(); //this is not ideal either
+			
+			//Add URL handling class stuff here
+			
+			String imageType = new String(src.normalName());
+			File file = new File(src.attr("src"));
+			try {
+				imageType = Files.probeContentType(file.toPath());
+			} catch (IOException e) {
+				System.out.print("File type not detected");
+				e.printStackTrace();
+			}
+			
+			
+//			if(url.contains(websiteDomain)) //if url domain matches website domain, it's internal and added to media
+//			{
+					//String convertedURL = stripUrl(url);
+					long size = (file.length() / 1048576); //convert Byte to MiB
+					double fileSize = (double) size;
+					String lfp = src.attr("src");
+					String name = file.getName();
+					//numPages??
+					FileResource newMedia = new FileResource(fileSize, 0, imageType, lfp, name);
+					htmldoc.setMedia(newMedia);
+//			}
 			}
 		}
-		*/
 	}
 	
 	/**
