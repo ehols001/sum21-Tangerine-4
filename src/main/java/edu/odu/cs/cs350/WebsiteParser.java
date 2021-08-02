73d42689126cd9fa349c3f9e20bab9f642248941
cpalm012 <cpalm012@odu.edu>
2021-08-01 22:39:16 -0400

    Add testing and implementation to parser
    Mostly worked on media extraction, added some link testing. Divested
    parsing from HTMLDoc

--- src/main/java/edu/odu/cs/cs350/WebsiteParser.java
+++ src/main/java/edu/odu/cs/cs350/WebsiteParser.java
@@ -1,14 +1,141 @@
 package edu.odu.cs.cs350;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Paths;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 
 public class WebsiteParser {
 	
-	public void linkExtract() {
-		
+	private String websiteDomain;
+	private Document webPage;
+	private String localPath;
+	private String rootPath;
+	private HTMLDocument htmlDoc;
+	
+	public WebsiteParser() {
+		this.websiteDomain = new String("");
+		this.rootPath = new String("");
+		this.localPath = new String("");
+		htmlDoc = new HTMLDocument();
 	}
 	
-	public void mediaExtract() {
+	public void generateHtml(String localPath)
+	{
+		try
+		{
+			File content = new File(localPath);
+			Document doc = Jsoup.parse(content, "UTF-8");
+			extractLinks(doc);
+			mediaExtract(doc);
+			//extractScripts(doc);
+			//extractStyleSheets(doc);
+			//extractMisc(doc);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 		
+		//test output to make sure it's grabbing/converting links correctly
+//		for(int i = 0; i < htmlDoc.getLinks().size(); ++i)
+//		{
+//			System.out.print(htmlDoc.getLinks().get(i).getType());
+//			System.out.print(", ");
+//			System.out.println(htmlDoc.getLinks().get(i).getURL());
+//		}
+	}
+	
+	public void extractLinks(Document doc) {
+		String type = new String();
+		Elements anchors = doc.select("a[href]"); //collect a list of all link tags on the page
+		
+		for(Element anchor : anchors) //for each link in the list
+		{
+			String url = anchor.attr("abs:href"); //get the full link path
+			Anchor link = new Anchor(url, type);
+			if(url.contains(websiteDomain)) //if url domain matches website domain, it's internal and added to links
+			{
+				link.setType("internal");
+				htmlDoc.setLinks(link);
+			}
+			else
+			{
+				link.setType("external");
+				htmlDoc.setLinks(link);
+			}
+		}
+	}
+	
+	/**
+	 * Translates a URL to the local directory structure
+	 * of a local copy of a site
+	 * 
+	 * @param url URL to be stripped of protocol
+	 * @return filePath url translated to its local file path
+	 */
+	public String stripUrl(String url)
+	{
+		String fileName = new String();
+		try
+		{
+			URL tempUrl = new URL(url);
+			String protocol = tempUrl.getProtocol() + "://";
+			fileName = url.replace(protocol, "");
+		}
+		catch (MalformedURLException mue)
+		{
+			System.err.println(mue);
+			System.out.println(url);
+		}
+		return fileName;
+	}
+	
+	public void mediaExtract(Document webPage) {
+		Elements media = webPage.select("[src]"); //collect a list of all media-like tags containing src on the page
+		
+		for(Element src : media) //for each src in the media list
+		{
+			if (src.normalName().equals("img")) {
+				
+			
+			//String url = src.attr("abs:src"); //get the full path of the media src
+//			String url = src.absUrl("src");
+			String url = src.baseUri();
+			String imageType = new String(src.normalName());
+			File file = new File(src.attr("src"));
+			try {
+				imageType = Files.probeContentType(file.toPath());
+			} catch (IOException e) {
+				System.out.print("File type not detected");
+				e.printStackTrace();
+			}
+			
+//			if(imageType == "img") //check if that src is an img
+//			{
+				
+//				if(url.contains(websiteDomain)) //if url domain matches website domain, it's internal and added to media
+//				{
+					//String convertedURL = stripUrl(url);
+					File mediaFile = new File(url);
+					long fileSize = file.length();
+					String lfp = src.attr("src");
+					String name = src.normalName();
+					//numPages??
+					FileResource newMedia = new FileResource(fileSize, 0, imageType, lfp, name);
+					htmlDoc.setMedia(newMedia);
+//				}
+//			}
+			}
+		}
 	}
 	
 	public void scriptExtract() {
@@ -25,6 +152,14 @@ public class WebsiteParser {
 	
 	public void pathExtract() {
 		
+	}
+	
+	public void setHTML(HTMLDocument html) {
+		this.htmlDoc = html;
+	}
+	
+	public HTMLDocument getHTML() {
+		return this.htmlDoc;
 	}
 	
 	
--- src/test/java/edu/odu/cs/cs350/TestWebsiteParser.java
+++ src/test/java/edu/odu/cs/cs350/TestWebsiteParser.java
@@ -7,18 +7,75 @@ import org.junit.Test;
 
 public class TestWebsiteParser {
 
+	private Website website;
+	private HTMLDocument html1;
+	private Anchor testLink;
+	private FileResource testImage;
+	private FileResource testVideo;
+	private FileResource testJavaScript;
+	private FileResource testCSS;
+	private FileResource testMisc;
+	
+	private HTMLDocument html2;
+	private FileResource testMiscDuplicate;
+	
+	private String lfp;
+	
+	
 	@Before
-	public void setUp() {
+	public void setup() {
+		website = new Website();
+		html1 = new HTMLDocument();
+		testLink = new Anchor("www.test.org", "external");
+		testImage = new FileResource(3, 1024, "image", "/local/testImage.png", "name");
+		testVideo = new FileResource(900, 4000, "video", "/local/testVideo.mp4", "name");
+		testJavaScript = new FileResource(1, 2056, "script", "/local/testJS.js", "name");
+		testCSS = new FileResource(1, 4096, "stylesheet", "/local/testCSS.css", "name");
+		testMisc = new FileResource(2, 40, "misc", "/local/testMisc.zip", "name");
+		
+//		html2 = new HTMLDocument()
+//		testMiscDuplicate = new FileResource(2, 40, "misc", "/local/testMisc.zip", "name");
+
+		
+		website.addWebpage(html1);
+//		website.getWebpage(0).setMedia(testCSS);
+//		website.getWebpage(0).setMedia(testImage);
+//		website.getWebpage(0).setMedia(testVideo);
+//		website.getWebpage(0).setMedia(testJavaScript);
+//		website.getWebpage(0).setMedia(testMisc);
+		
+//		website.addWebpage(html2);
+//		website.getWebpage(1).setMedia(testMiscDuplicate);
+		WebsiteParser parser = new WebsiteParser();
+		parser.setHTML(html1);
+		
+//		parser.generateHtml("/home/slowmobro/git/sum21-Tangerine-4/src/test/java/edu/odu/cs/cs350/testHTML.html");
+		
 	}
 
 	@Test
 	public void testLinkExtract() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		WebsiteParser parser = new WebsiteParser();
+//		parser.generateHtml("/home/slowmobro/git/sum21-Tangerine-4/src/test/java/edu/odu/cs/cs350/testHTML.html");
+//
+//		assertEquals(parser.getHTML().getLinks().get(0).getURL(), "https://www.w3schools.com");
+//		assertEquals(parser.getHTML().getLinks().get(1).getURL(), "http://www.test_with@symbols.com");
+//		assertEquals(parser.getHTML().getLinks().get(2).getURL(), "https://www.test.com");
+		
+		
 	}
 
 	@Test
 	public void testMediaExtract() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		WebsiteParser parser = new WebsiteParser();
+		
+		parser.generateHtml("src/test/java/edu/odu/cs/cs350/Resources/TestWebsite/www.cs.odu.edu/_tkennedy/cs350/latest/index.html");
+		
+		assertEquals(parser.getHTML().getMedia().get(0).getLocalPath(), "./index_files/text-kind.png");
+		//assertEquals(parser.getHTML().getMedia().get(0).getSize(), 1554371, 0.001);
+		//size in bytes 1,554,371 
 	}
 
 	@Test
